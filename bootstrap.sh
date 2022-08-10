c93d154e11799a31c18b1dff4319b2e56b986ed2
root <root@ip-172-31-58-94.us-west-2.compute.internal>
2022-08-10 08:52:07 +0000

    support puppet_node_classifier

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,15 @@
 
 PUPPETPATH="/etc/puppetlabs"
 
+echo "sync puppet server basic config"
 git clone https://github.com/kaija/puppet-composer.git $PUPPETPATH
 
 cd $PUPPETPATH
+
+echo "build puppet server container"
+
+docker-compose build
+
+echo "launch puppet server"
 
 docker-compose up -d
